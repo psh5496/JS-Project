bd3f56c8f5b58969a72465b1907750174f04e5da
seonghyeon <shp35079@naver.com>
2021-06-01 20:28:06 +0900

    [UPDATE] numButton click complete
    .

--- pagination/script.js
+++ pagination/script.js
@@ -1,28 +1,27 @@
-const $pageNum = document.getElementsByClassName('page-button');
+const $pageNumButtonArr = document.getElementsByClassName('page-button');
 const PAGE_PER_BAR = 5; // 한 줄 당 표시되는 페이지 수
 const DATA_PER_PAGE = 8; // 페이지 당 표시할 데이터 수
 let posts; // api를 통해 받아온 객체 배열
 let totalPage; // 총 페이지 수
 const DOMAIN = 'https://jsonplaceholder.typicode.com/';
 
-const getPageNum = (currentPage) => {
+const setPageNum = (currentPage) => {
     const half = Math.floor(PAGE_PER_BAR / 2);
     let start;
-    if(currentPage < 3) {
+    if (currentPage <= half) {
         start = 1;
-    } else if(currentPage > totalPage - half) {
-        start = (totalPage - PAGE_PER_BAR) + 1;
+    } else if (currentPage > totalPage - half) {
+        start = totalPage - PAGE_PER_BAR + 1;
     } else {
         start = currentPage - half;
     }
-    setPageNum(Array.from({length: PAGE_PER_BAR}, (_, i) => start + i));
-};
 
-const setPageNum = (pages) => {
-    for(let i=0; i < PAGE_PER_BAR; i++) {
-        $pageNum[i].innerText = pages[i];
+    const pages = Array.from({ length: PAGE_PER_BAR }, (_, i) => start + i);
+
+    for (let i = 0; i < PAGE_PER_BAR; i++) {
+        $pageNumButtonArr[i].innerText = pages[i];
     }
-}
+};
 
 const setPageInfo = ({ data }) => {
     posts = data;
@@ -36,12 +35,19 @@ const getPosts = async function () {
 
 getPosts();
 
+const clickPageNumButton = (i) => {
+    setPageNum($pageNumButtonArr[i].innerText);
+    for (let j = 0; j < PAGE_PER_BAR; j++) {
+        $pageNumButtonArr[j].classList.remove('active');
+    }
+
+    if (2 < $pageNumButtonArr[i].innerText && $pageNumButtonArr[i].innerText < 12) {
+        $pageNumButtonArr[2].classList.add('active');
+    } else {
+        $pageNumButtonArr[i].classList.add('active');
+    }
+};
+
 for (let i = 0; i < PAGE_PER_BAR; i++) {
-    $pageNum[i].addEventListener('click', () => {
-        getPageNum($pageNum[i].innerText);
-        for (let j = 0; j < PAGE_PER_BAR; j++) {
-            $pageNum[j].classList.remove('active');
-        }
-        $pageNum[i].classList.add('active');
-    });
+    $pageNumButtonArr[i].addEventListener('click', () => clickPageNumButton(i));
 }
