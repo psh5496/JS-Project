9863bc7e64bca62a55e4d9ac3d31c304bdb0573a
seonghyeon <shp35079@naver.com>
2021-06-02 17:20:51 +0900

    [UPDATE] Code Refactoring
    .

--- pagination/script.js
+++ pagination/script.js
@@ -1,11 +1,32 @@
 const $pageNumButtonArr = document.getElementsByClassName('page-button');
 const PAGE_PER_BAR = 5; // 한 줄 당 표시되는 페이지 수
 const DATA_PER_PAGE = 8; // 페이지 당 표시할 데이터 수
-let posts; // api를 통해 받아온 객체 배열
-let totalPage; // 총 페이지 수
 const DOMAIN = 'https://jsonplaceholder.typicode.com/';
+const postsObj = (function () {
+    let posts = [];
+    return {
+        setPosts: function (_posts) {
+            posts = _posts;
+        },
+        getPosts: function () {
+            return posts;
+        },
+    };
+})();
+const totalPageObj = (function () {
+    let totalPage = 0;
+    return {
+        setTotalPage: function (_totalPage) {
+            totalPage = _totalPage;
+        },
+        getTotalPage: function () {
+            return totalPage;
+        },
+    };
+})();
 
 const setPageNum = (currentPage) => {
+    const totalPage = totalPageObj.getTotalPage();
     const half = Math.floor(PAGE_PER_BAR / 2);
     let start;
     if (currentPage <= half) {
@@ -17,23 +38,20 @@ const setPageNum = (currentPage) => {
     }
 
     const pages = Array.from({ length: PAGE_PER_BAR }, (_, i) => start + i);
-
     for (let i = 0; i < PAGE_PER_BAR; i++) {
         $pageNumButtonArr[i].innerText = pages[i];
     }
 };
 
-const setPageInfo = ({ data }) => {
-    posts = data;
-    totalPage = Math.ceil(posts.length / DATA_PER_PAGE); // 총 페이지 수
+const setPageInfo = ({ data: posts }) => {
+    postsObj.setPosts(posts);
+    totalPageObj.setTotalPage(Math.ceil(posts.length / DATA_PER_PAGE));
 };
 
-const getPosts = async function () {
+const fetchPosts = async function () {
     const response = await axios.get(DOMAIN + 'posts');
     setPageInfo(response);
 };
-
-getPosts();
 
 const clickPageNumButton = (i) => {
     setPageNum($pageNumButtonArr[i].innerText);
@@ -51,3 +69,5 @@ const clickPageNumButton = (i) => {
 for (let i = 0; i < PAGE_PER_BAR; i++) {
     $pageNumButtonArr[i].addEventListener('click', () => clickPageNumButton(i));
 }
+
+fetchPosts();
